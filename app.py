13ca239b0255d5d0c51ad97b741a09e9a7295615
Juan Luis Cano Rodríguez <hello@juanlu.space>
2022-06-29 22:57:52 +0200

    Add OAuth 2 flow for Meetup

--- app.py
+++ app.py
@@ -1,6 +1,13 @@
+import os
+
 from flask import Flask, render_template, request, url_for, redirect
+from flask_dance.contrib.meetup import make_meetup_blueprint, meetup
 
 app = Flask(__name__)
+app.secret_key = os.environ["FLASK_SECRET_KEY"]
+
+blueprint = make_meetup_blueprint()
+app.register_blueprint(blueprint, url_prefix="/login")
 
 
 @app.route("/")
@@ -15,6 +22,12 @@ def health():
 
 @app.route("/checkin", methods=["GET", "POST"])
 def checkin():
+    if not meetup.authorized:
+        return redirect(url_for("meetup.login"))
+    resp = meetup.get("/members/self")
+    assert resp.ok
+    print(resp.json())
+
     if request.method == "POST":
         print(request.form)
         return redirect(url_for("thankyou"))
